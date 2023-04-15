509de4fd3c9480db49ddfd65177717ea7c443462
Tinku Hore <tinku.doitnow@gmail.com>
2023-04-15 22:53:51 +0530

    spelling correction

--- template.py
+++ template.py
@@ -27,7 +27,7 @@ list_of_files = [
     f"{project_name}/constant/training_pipeline/application.py",
 
     f"{project_name}/entity/__init__.py",
-    f"{project_name}/entity/artifacts_entity.py",
+    f"{project_name}/entity/artifact_entity.py",
     f"{project_name}/entity/config_entity.py",
 
     f"{project_name}/exception/_init__.py",
