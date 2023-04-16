fe7a75f90027ba2257104def36a6808a23f58394
Tinku Hore <tinku.doitnow@gmail.com>
2023-04-16 13:58:56 +0530

    exception script

--- signLanguage/exception/__init__.py
+++ signLanguage/exception/__init__.py
@@ -2,13 +2,11 @@ import sys
 
 
 def error_message_detail(error, error_detail: sys):
-    _, _, exc_tb = error_detail.exc_info()
+    _, _, exc_tb= error_detail.exc_info()
 
     file_name = exc_tb.tb_frame.f_code.co_filename
 
-    error_message = "Error occurred python script name [{0}] line number [{1}] error message [{2}]".format(
-        file_name, exc_tb.tb_lineno, str(error)
-    )
+    error_message = "Error encountered python script name [{0}] line number: [{1}] error message [{2}]".format(file_name, exc_tb.tb_lineno, str(error))
 
     return error_message
 
@@ -16,10 +14,10 @@ def error_message_detail(error, error_detail: sys):
 class SignException(Exception):
     def __init__(self, error_message, error_detail):
         """
-        :param error_message: error message in string format
+        :param error_message: error message in str format
+        :param error_detail:
         """
         super().__init__(error_message)
-
         self.error_message = error_message_detail(
             error_message, error_detail=error_detail
         )
