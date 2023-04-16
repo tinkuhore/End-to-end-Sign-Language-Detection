b7bdf05fd1927d0df180890931684e8099d4df1e
Tinku Hore <tinku.doitnow@gmail.com>
2023-04-16 13:04:38 +0530

    updated personal info

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import find_packages, setup
 
 setup(
     name = 'signLanguages',
-    version= '0.0.0',
-    author= 'iNeuron',
-    author_email= 'boktiar@ineuron.ai',
+    version= '0.0.1',
+    author= 'TinkuHore',
+    author_email= 'tinku.doitnow@gmail.com',
     packages= find_packages(),
     install_requires = []
 
