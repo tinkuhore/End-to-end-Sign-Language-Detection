b06d64c7f3d67de8ff5819469dbf1c0e8114118e
Tinku Hore <tinku.doitnow@gmail.com>
2023-04-16 14:17:00 +0530

    created helper functons

--- signLanguage/utils/main_utils.py
+++ signLanguage/utils/main_utils.py
@@ -1,4 +1,4 @@
-import os.path
+import os
 import sys
 import yaml
 import base64
@@ -7,17 +7,16 @@ from signLanguage.exception import SignException
 from signLanguage.logger import logging
 
 
-def read_yaml_file(file_path: str) -> dict:
+def read_yaml_file(file_path: str) ->dict:
     try:
-        with open(file_path, "rb") as yaml_file:
-            logging.info("Read yaml file successfully")
-            return yaml.safe_load(yaml_file)
-
+        with open(file_path, 'rb') as stream:
+            logging.info("Reading yaml file")
+            return yaml.load(stream)
     except Exception as e:
-        raise SignException(e, sys) from e
+        raise SignException(e, sys)
     
 
-def write_yaml_file(file_path: str, content: object, replace: bool = False) -> None:
+def write_yaml_file(file_path: str, content: object, replace: bool = False) ->None:
     try:
         if replace:
             if os.path.exists(file_path):
@@ -25,23 +24,23 @@ def write_yaml_file(file_path: str, content: object, replace: bool = False) -> N
 
         os.makedirs(os.path.dirname(file_path), exist_ok=True)
 
-        with open(file_path, "w") as file:
+        with open(file_path, 'w') as file:
             yaml.dump(content, file)
-            logging.info("Successfully write_yaml_file")
+            logging.info("Writing yaml file successfully")
 
     except Exception as e:
         raise SignException(e, sys)
     
 
-
-
-def decodeImage(imgstring, fileName):
+def decodeImage(imgstring, filename: str):
     imgdata = base64.b64decode(imgstring)
-    with open("./data/" + fileName, 'wb') as f:
+    with open("./data/" + filename, 'wb') as f:
         f.write(imgdata)
         f.close()
 
 
-def encodeImageIntoBase64(croppedImagePath):
-    with open(croppedImagePath, "rb") as f:
-        return base64.b64encode(f.read())
+def encodeImage(cropped_img_path: str):
+    with open(cropped_img_path, 'rb') as f:
+        imgdata = f.read()
+        imgstring = base64.b64encode(imgdata)
+        return imgstring
