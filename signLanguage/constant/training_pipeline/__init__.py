220d6c95643ebe44ca18dee2eafd5d154425dece
Tinku Hore <tinku.doitnow@gmail.com>
2023-04-16 14:42:25 +0530

    data ingestion component

---
+++ signLanguage/constant/training_pipeline/__init__.py
@@ -0,0 +1,10 @@
+import os
+
+ARTIFACTS_DIR : str = "artifacts"
+
+'''
+data ingestion related constants
+'''
+DATA_INGESTION_DIR_NAME : str = "data_ingestion"
+DATA_INGESTION_FEATURE_STORE_DIR : str = "features_store"
+DATA_DOWNLOAD_URL: str = "<git_url>"
