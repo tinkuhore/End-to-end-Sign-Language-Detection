5af587d00d0ce5ffb6be64b57ee4f20d92dce884
Tinku Hore <tinku.doitnow@gmail.com>
2023-04-16 13:48:48 +0530

    logger script

--- signLanguage/logger/__init__.py
+++ signLanguage/logger/__init__.py
@@ -4,16 +4,16 @@ from datetime import datetime
 from from_root import from_root
 
 
-LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+LOG_FILE_NAME = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 
-log_path = os.path.join(from_root(), 'log', LOG_FILE)
+log_path = os.path.join(from_root(), 'log', LOG_FILE_NAME)
 
 os.makedirs(log_path, exist_ok=True)
 
-lOG_FILE_PATH = os.path.join(log_path, LOG_FILE)
+LOG_FILE_PATH = os.path.join(log_path, LOG_FILE_NAME)
 
 logging.basicConfig(
-    filename=lOG_FILE_PATH,
-    format= "[ %(asctime)s ] %(name)s - %(levelname)s - %(message)s",
-    level= logging.INFO
+    filename=LOG_FILE_PATH,
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
 )
