e291f2fc0fd8ff0925c7d9bacf187d12ee8b7edc
Tinku Hore <tinku.doitnow@gmail.com>
2023-04-19 20:03:06 +0530

    data download URL of git repo added

--- signLanguage/constant/training_pipeline/__init__.py
+++ signLanguage/constant/training_pipeline/__init__.py
@@ -7,4 +7,4 @@ data ingestion related constants
 '''
 DATA_INGESTION_DIR_NAME : str = "data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR : str = "features_store"
-DATA_DOWNLOAD_URL: str = "<git_url>"
+DATA_DOWNLOAD_URL: str = "https://github.com/entbappy/Branching-tutorial/raw/master/Sign_language_data.zip"
